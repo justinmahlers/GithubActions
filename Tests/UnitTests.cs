8be6284d87d2e2f14afae4fbe60132a1d0d451b6
justinmahlers <90645115+justinmahlers@users.noreply.github.com>
2022-11-09 16:19:21 -0600

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -113,6 +113,7 @@ namespace GithubActionsLab
             Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
         }
+        
         // Implement 3 tests per operation, following a similar pattern as above
     }
 }
