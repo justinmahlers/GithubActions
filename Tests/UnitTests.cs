595ed965bb4a9326603a47a2f02160bddce533f8
justinmahlers <90645115+justinmahlers@users.noreply.github.com>
2022-11-09 15:46:12 -0600

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -29,7 +29,90 @@ namespace GithubActionsLab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
-
+        [Test]
+        public void Subtract_Valid()
+        {
+            Assert.AreEqual(7, Program.Subtract("10","3"));
+            Assert.AreEqual(16, Program.Subtract("20","4"));
+            Assert.AreEqual(3, Program.Subtract("4","3"));
+        }
+        [Test]
+        public void Subtract_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+        [Test]
+        public void Subtract_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", "null"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("null", "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("null", "null"));
+        }
+        [Test]
+        public void Multiply_Valid()
+        {
+            Assert.AreEqual(6, Program.Multiply("2","3"));
+            Assert.AreEqual(40, Program.Multiply("20","2"));
+            Assert.AreEqual(12, Program.Multiply("4","3"));
+        }
+        [Test]
+        public void Multiply_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+        [Test]
+        public void Multiply_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", "null"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("null", "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("null", "null"));
+        }
+        [Test]
+        public void Divide_Valid()
+        {
+            Assert.AreEqual(6, Program.Divide("18","3"));
+            Assert.AreEqual(40, Program.Divide("80","2"));
+            Assert.AreEqual(12, Program.Divide("36","3"));
+        }
+        [Test]
+        public void Divide_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+        [Test]
+        public void Dividey_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", "null"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("null", "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("null", "null"));
+        }
+        [Test]
+        public void Power_Valid()
+        {
+            Assert.AreEqual(8, Program.Power("2","3"));
+            Assert.AreEqual(9, Program.Power("3","2"));
+            Assert.AreEqual(16, Program.Power("4","2"));
+        }
+        [Test]
+        public void Power_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+        [Test]
+        public void Power_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", "null"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power("null", "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power("null", "null"));
+        }
         // Implement 3 tests per operation, following a similar pattern as above
     }
 }
