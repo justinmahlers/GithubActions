23664ed3026d241ce9584c6f0c54c0783f67a54d
justinmahlers <90645115+justinmahlers@users.noreply.github.com>
2022-11-09 12:02:00 -0600

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -85,7 +85,7 @@ namespace GithubActionsLab
         public static double Power(string x, string y)
         {
             return double.Parse(x) ^ double.Parse(y);
-            throw new NotImplementedException();
+            
         }
     }
 
